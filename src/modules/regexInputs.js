6c6869c97467ba59085d09a832904fc7a5c99b1a
Thomas Bnt <thomasbnt@protonmail.com>
2022-09-08 11:47:21 +0200

    validateEmailAndPassword > regexInputs

---
+++ src/modules/regexInputs.js
@@ -0,0 +1,25 @@
+// TODO : Message resp Err 400 si input non conforme.
+class RegexInputs {
+    checkEmail(email) {
+        if (email === undefined || email === '') {
+            return false;
+        }
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    }
+
+    checkPassword(password) {
+        if (password === undefined || password === '') return false
+        // Simple système de Regex pour vérifier le mot de passe.
+        // Min 9 caractères, une lettre en majuscule, un caractère spécial et un chiffre.
+        const regPassword = new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$')
+        return regPassword.test(password);
+    }
+
+    checkText(input) {
+        if (input === undefined || input === '') return false;
+
+    }
+
+}
+
+module.exports = new RegexInputs();
