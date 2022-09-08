8744b690e526ac669d13e6a540983213d0cfeea7
Thomas Bnt <thomasbnt@protonmail.com>
2022-09-08 11:51:49 +0200

    Conditions checkIf inputs are good in signup

--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -19,36 +19,11 @@ exports.UsersSignup = (req, res) => {
     const lastNameIsValid = regexInputs.checkText(lastName);
 
     if (emailIsValid && passwordIsValid && firstNameIsValid && lastNameIsValid) {
-        // si l'email est déjà dans la base de donnée, alors erreur
-        signup.findOne({
-            email: email
-        }).then(async findEmail => {
-            if (findEmail) {
-                console.log('Email already exists');
-                return resp.error('Email already exists', res);
-            }
-            if (findEmail === null) {
-                // chiffre le password
-                const hashPassword = await hash.gen(password)
-                // si l'email n'est pas dans la base de donnée, alors on crée un nouvel utilisateur
-                const newUser = new signup({
-                    email: email,
-                    password: hashPassword
-                });
-                newUser.save()
-                    .then(() => {
-                        console.log('A new signup has been created');
-                        resp.success('Success: You are now signed up on Hot Takes.', res);
-                    })
-                    .catch(err => {
-                        console.error(err);
-                        resp.error("An error occurred while creating a new signup", res);
-                    });
-            }
-        })
+
     } else {
-        console.log('Email or password is not valid');
-        !emailIsValid ? resp.error('Error: Email is required or you typed it wrong.', res) : null;
-        !passwordIsValid ? resp.error('Error: Password is required (Make sure that you put at least 6 characters for security reasons).', res) : null;
+        !emailIsValid ? resp.badRequest('L\'email que vous avez entré n\'est pas valide', res) : null;
+        !passwordIsValid ? resp.badRequest('Le mot de passe doit être au minimum de 8 caractères comprenant un caractère spécial, un chiffre, une lettre majuscule.', res) : null;
+        !firstName ? resp.badRequest('Votre prénom n\'est pas valide', res) : null
+        !lastName ? resp.badRequest('Votre nom n\'est pas valide', res) : null
     }
 }
