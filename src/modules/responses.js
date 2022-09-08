e6aef84f5f98c68d0e0cd4e216b77d9f7df5e5cd
Thomas Bnt <thomasbnt@protonmail.com>
2022-09-08 09:52:44 +0200

    Module for header responses

---
+++ src/modules/responses.js
@@ -0,0 +1,51 @@
+class Responses {
+    success(message, res) {
+        return res.status(200).json({
+            success: message || 'Success'
+        });
+    }
+
+    created(message, res) {
+        return res.status(201).json({
+            success: message || 'Created'
+        });
+    }
+
+    error(message, res) {
+        return res.status(400).json({
+            error: message || 'Error'
+        });
+    }
+
+    invalidCredentials(message, res) {
+        return res.status(401).json({
+            error: message || 'Invalid credentials'
+        });
+    }
+
+    forbidden(message, res) {
+        return res.status(403).json({
+            error: message || 'Forbidden'
+        });
+    }
+
+    notFound(res) {
+        return res.status(404).json({
+            error: 'Not found'
+        });
+    }
+
+    conflict(message, res) {
+        return res.status(409).json({
+            error: message || 'Conflict!'
+        });
+    }
+
+    internalError(message, res) {
+        return res.status(500).json({
+            error: message || 'Internal error'
+        });
+    }
+}
+
+module.exports = new Responses();
