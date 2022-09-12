92ae5f0ae817f85bc09366bbdd31d2fd0419dda1
Thomas Bnt <thomasbnt@protonmail.com>
2022-09-12 19:17:18 +0200

    Added initial routes for posts

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -6,7 +6,26 @@ const PostCtrl = require('../controllers/posts')
 const upload = require('../middlewares/multer')
 const auth = require('../middlewares/authenticateToken')
 
-// Get All posts
-router.get('/', cors({ methods: 'GET' }), PostCtrl.PostsGetAll)
+// Créer un nouveau post
+router.post(
+  '/',
+  cors({ methods: 'POST' }),
+  auth,
+  upload,
+  PostCtrl.PostsCreateOne
+)
+// Récupérer tous les posts
+router.get('/', cors({ methods: 'GET' }), auth, PostCtrl.PostsGetAll)
+// Récupérer un seul post
+router.get('/:id', cors({ methods: 'GET' }), auth, PostCtrl.PostsGetOne)
+// Mettre à jour un seul post
+router.put('/:id', cors({ methods: 'PUT' }), auth, PostCtrl.PostsUpdateOne)
+// Supprimer un seul post
+router.delete(
+  '/:id',
+  cors({ methods: 'DELETE' }),
+  auth,
+  PostCtrl.PostsDeleteOne
+)
 
 module.exports = router
