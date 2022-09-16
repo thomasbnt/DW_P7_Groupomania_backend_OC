7ae8cc3ca5de60b2cd10ebf21450bbdfc719d0d2
Thomas Bnt <thomasbnt@protonmail.com>
2022-09-16 21:28:50 +0200

    Better conditions /signup route

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -3,90 +3,92 @@ const regexInputs = require('../modules/regexInputs')
 const hash = require('../middlewares/hash')
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
+const { user: User } = prisma
 
 // Signup and login part
 exports.UsersSignup = async (req, res) => {
   console.log('Signup request received')
   // Vérifier si le contenu account n'est pas vide dans un premier temps
-  if (req.body.account) {
-    console.log(req.file.filename)
-    try {
-      const account = JSON.parse(req.body.account)
-      console.log({ firstName: account.firstName })
+  try {
+    const account = JSON.parse(req.body.account)
+    if (
+      !account &&
+      !account.email &&
+      !account.password &&
+      !account.lastName &&
+      !account.firstName
+    )
+      return resp.badRequest(
+        'Il manque des informations pour vous enregistrer. Veuillez bien fournir tout les champs.',
+        res
+      )
+    if (!req.file) return resp.badRequest('Veuillez ajouter une image', res)
 
-      if (
-        account &&
-        account.email &&
-        account.password &&
-        account.lastName &&
-        account.firstName &&
-        req.file
-      ) {
-        const { email, password, firstName, lastName } = account
-        const profileImage = req.file.filename
-        console.log({ profileImage })
+    const { email, password, firstName, lastName } = account
+    console.log({ email, password, firstName, lastName })
 
-        const emailIsValid = regexInputs.checkEmail(email)
-        const passwordIsValid = regexInputs.checkPassword(password)
-        const firstNameIsValid = regexInputs.checkText(firstName)
-        const lastNameIsValid = regexInputs.checkText(lastName)
+    // On vérifie si tous les champs sont bien remplis et si les regex sont respectés
+    const emailIsValid = regexInputs.checkEmail(email)
+    const passwordIsValid = regexInputs.checkPassword(password)
+    const firstNameIsValid = regexInputs.checkText(firstName)
+    const lastNameIsValid = regexInputs.checkText(lastName)
+    console.log({
+      emailIsValid,
+      passwordIsValid,
+      firstNameIsValid,
+      lastNameIsValid,
+    })
 
-        if (
-          emailIsValid &&
-          passwordIsValid &&
-          firstNameIsValid &&
-          lastNameIsValid
-        ) {
-          const userAlreadyExists = await prisma.user.findUnique({
-            where: { email: email },
-          })
-          console.log({ userAlreadyExists })
-          if (userAlreadyExists) {
-            await prisma.user.create({
-              data: {
-                firstName: firstName,
-                lastName: lastName,
-                email: email,
-                password: await hash.gen(password),
-                profileImage: profileImage,
-              },
-            })
-          }
-        } else {
-          !emailIsValid
-            ? resp.badRequest(
-                "L'email que vous avez entré n'est pas valide",
-                res
-              )
-            : null
-          !passwordIsValid
-            ? resp.badRequest(
-                'Le mot de passe doit être au minimum de 8 caractères comprenant un caractère spécial, un chiffre, une lettre majuscule.',
-                res
-              )
-            : null
-          !firstName
-            ? resp.badRequest("Votre prénom n'est pas valide", res)
-            : null
-          !lastName ? resp.badRequest("Votre nom n'est pas valide", res) : null
-        }
-      } else {
-        resp.badRequest(
-          'Il manque des informations pour vous enregistrer. Veuillez bien fournir tout les champs.',
-          res
-        )
-      }
-    } catch (error) {
-      console.log(error)
-      resp.badRequest('Le contenu de la requête est incorrect', res)
+    if (!emailIsValid)
+      return resp.badRequest(
+        "L'email que vous avez entré n'est pas valide",
+        res
+      )
+    if (!passwordIsValid)
+      return resp.badRequest(
+        'Le mot de passe doit être au minimum de 8 caractères comprenant un caractère spécial, un chiffre, une lettre majuscule.',
+        res
+      )
+    if (!firstNameIsValid)
+      return resp.badRequest(
+        "Le prénom que vous avez entré n'est pas valide",
+        res
+      )
+    if (!lastNameIsValid)
+      return resp.badRequest("Le nom que vous avez entré n'est pas valide", res)
+
+    const userFindUniqueByEmail = await prisma.user.findUnique({
+      where: { email: email },
+    })
+    console.log({ userFindUniqueByEmail })
+    if (userFindUniqueByEmail)
+      return resp.badRequest(
+        'Le compte avec cette adresse email existe déjà. Veuillez vous connecter.',
+        res
+      )
+    if (!userFindUniqueByEmail) {
+      await prisma.user.create({
+        data: {
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          password: await hash.gen(password),
+          imageProfile: `${req.protocol}://${req.get('host')}/images/${
+            req.file.filename
+          }`,
+        },
+      })
+      resp.success('Compte créé avec succès', res)
     }
-  } else {
+  } catch (error) {
+    console.log({ error })
     resp.badRequest(
-      'Il manque des informations pour vous enregistrer. Veuillez bien fournir tout les champs.',
+      'Veuillez bien remplir vos données de compte utilisateur.',
       res
     )
   }
 }
+
 exports.UsersLogin = async (req, res) => {
   console.log('Login request received')
   const account = JSON.parse(req.body.account)
