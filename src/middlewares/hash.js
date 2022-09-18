4f71b08fbc0a9b405a9d56d60c704ad702342bb8
Thomas Bnt <thomasbnt@protonmail.com>
2022-09-18 02:09:56 +0200

    Added compare and genToken functions hash

--- src/middlewares/hash.js
+++ src/middlewares/hash.js
@@ -1,9 +1,18 @@
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const salt = bcrypt.genSaltSync(10)
 class Hash {
   // hash password
   async gen(password) {
     return await bcrypt.hash(password, salt)
+  }
+  // compare password
+  async compare(password, hash) {
+    return await bcrypt.compare(password, hash)
+  }
+  // generate token
+  async genToken(user) {
+    return await jwt.sign({ user }, process.env.JWT_SECRET_KEY)
   }
 }
 
