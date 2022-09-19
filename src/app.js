92b9e970368bcf87bf808ef728e4ce225b6da455
Thomas Bnt <thomasbnt@protonmail.com>
2022-09-19 19:03:59 +0200

    CORS origin *

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-const express = require('express')
+const express = require("express")
 const app = express()
-require('dotenv').config()
-const cors = require('cors')
-const path = require('path')
-app.disable('x-powered-by')
+require("dotenv").config()
+const cors = require("cors")
+const path = require("path")
+app.disable("x-powered-by")
 
 app.use(
   express.urlencoded({
@@ -12,29 +12,29 @@ app.use(
 )
 
 app.use(express.json())
-app.use(cors({ origin: process.env.FRONTEND_URL || 'same-origin' }))
+app.use(cors({ origin: "*" || process.env.FRONTEND_URL || "same-origin" }))
 
-app.use('/images', express.static(path.join(__dirname, 'images')))
+app.use("/images", express.static(path.join(__dirname, "images")))
 
 app.use((req, res, next) => {
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
   )
-  res.setHeader('Accept', 'application/json, multipart/form-data')
+  res.setHeader("Accept", "application/json, multipart/form-data")
   next()
 })
 
 // Route Status
-const status = require('./routes/status')
-app.use('/status', status)
+const status = require("./routes/status")
+app.use("/status", status)
 
 // Route Users
-const users = require('./routes/users')
-app.use('/users', users)
+const users = require("./routes/users")
+app.use("/users", users)
 
 // Route Posts
-const posts = require('./routes/posts')
-app.use('/posts', posts)
+const posts = require("./routes/posts")
+app.use("/posts", posts)
 
 module.exports = app
