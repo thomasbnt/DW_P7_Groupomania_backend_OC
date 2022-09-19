c66b498ac5105b983fa81575b904577f4a4a14fd
Thomas Bnt <thomasbnt@protonmail.com>
2022-09-19 19:05:07 +0200

    put to post for edit public info of user

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,29 +1,42 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
-const cors = require('cors')
+const cors = require("cors")
 
-const UserCtrl = require('../controllers/users')
-const uploadImageProfile = require('../middlewares/multerImagesProfiles')
-const auth = require('../middlewares/authenticateToken')
+const UserCtrl = require("../controllers/users")
+const uploadImageProfile = require("../middlewares/multerImagesProfiles")
+const auth = require("../middlewares/authenticateToken")
 
 // Signup
 router.post(
-  '/signup',
-  cors({ methods: 'POST' }),
-  uploadImageProfile.single('image'),
+  "/signup",
+  cors({ methods: "POST" }),
+  uploadImageProfile.single("image"),
   UserCtrl.UsersSignup
 )
 
 // Login
-router.post('/login', cors({ methods: 'POST' }), UserCtrl.UsersLogin)
+router.post("/login", cors({ methods: "POST" }), UserCtrl.UsersLogin)
 
 // Me
-router.get('/me', cors({ methods: 'GET' }), auth, UserCtrl.UsersMeGet)
-router.put('/me', cors({ methods: 'PUT' }), auth, UserCtrl.UsersMePut)
-router.delete('/me', cors({ methods: 'PUT' }), auth, UserCtrl.UsersSecurity)
-router.put(
-  '/security',
-  cors({ methods: 'DELETE' }),
+router.get("/me", cors({ methods: "GET" }), auth, UserCtrl.UsersMeGet)
+router.post(
+  "/me",
+  cors({ methods: "POST" }),
+  auth,
+  uploadImageProfile.single("image"),
+  UserCtrl.UsersMePost
+)
+
+// Security
+router.post(
+  "/security",
+  cors({ methods: "POST" }),
+  auth,
+  UserCtrl.UsersSecurity
+)
+router.delete(
+  "/security",
+  cors({ methods: "DELETE" }),
   auth,
   UserCtrl.UsersMeDelete
 )
