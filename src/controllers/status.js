5cef463f59ba58007a42aa9c67e149d2a949a28e
Thomas Bnt <thomasbnt@protonmail.com>
2022-09-19 22:49:16 +0200

    Added some info /status like date and stats

--- src/controllers/status.js
+++ src/controllers/status.js
@@ -1,5 +1,20 @@
 const resp = require("../modules/responses")
+const { PrismaClient } = require("@prisma/client")
+const prisma = new PrismaClient()
+
 exports.status = async (req, res) => {
   console.log("status route has been called")
-  resp.success("Tout est up!", res)
+  const postsCount = await prisma.post.count()
+  const usersCount = await prisma.user.count()
+  res.status(200).json({
+    status: "Tout est up!",
+    date: {
+      serverDate: new Date(),
+      serverDateFormatted: new Date().toLocaleString(),
+      serverDateFormattedParis: new Date().toLocaleString("fr-FR", {
+        timeZone: "Europe/Paris",
+      }),
+    },
+    stats: { postsCount, usersCount },
+  })
 }
